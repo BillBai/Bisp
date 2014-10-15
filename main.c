b195edfff3a8e8c5008815a51d89cddce941fc03
BillBai <billbai42@gmail.com>
2014-10-15 20:44:04 +0800

    parsing

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "Assets/mpc.h"
+
 #ifdef _WIN32
 
 #include <string.h>
@@ -30,17 +32,43 @@ void add_history(char *input) {}
 
 int main(int argc, char** argv)
 {
-   puts("Bisp version 0.0.0.1");
-   puts("Press ctrl-c to exit\n");
+    /* Create parsers */
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Bisp = mpc_new("bisp");
 
-   while (1) {
-       char *input = readline("Bisp :> ");
+    mpca_lang(MPCA_LANG_DEFAULT,
+            "                                                    \
+            number : /-?[0-9]+/ ;                                \
+            operator : '+' | '-' | '*' | '/' | '%' ;             \
+            expr : <number> | '(' <operator> <expr>+ ')' ;       \
+            bisp : /^/ <operator> <expr>+ /$/ ;                  \
+            ",
+            Number, Operator, Expr, Bisp);
 
-       add_history(input);
+    puts("Bisp version 0.0.0.1");
+    puts("Press ctrl-c to exit\n");
 
-       printf("You are %s\n", input);
+    while (1) {
+        char *input = readline("Bisp :> ");
+        add_history(input);
 
-       free(input);
-   }
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Bisp, &r))
+        {
+           mpc_ast_print(r.output);
+           mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
+
+
+        free(input);
+    }
+
+    mpc_cleanup(4, Number, Operator, Expr, Bisp);
 }
 
