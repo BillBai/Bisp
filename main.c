f8338946ba712d1b9ebfa98a5ac5855987c46f90
BillBai <billbai42@gmail.com>
2014-10-15 09:05:52 +0800

    readline to add history and edit inputs

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-static char input[2048];
+#ifdef _WIN32
+
+#include <string.h>
+
+static char buffer[2048];
+
+char* readline(char* promt)
+{
+    fputs(promt);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+    return cpy;
+}
+
+// Fake add_history for _WIN32
+void add_history(char *input) {}
+
+#else
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#endif
+
 
 int main(int argc, char** argv)
 {
@@ -8,10 +34,13 @@ int main(int argc, char** argv)
    puts("Press ctrl-c to exit\n");
 
    while (1) {
-       fputs("bisp :> ", stdout);
+       char *input = readline("Bisp :> ");
 
-       fgets(input, 20048, stdin);
+       add_history(input);
 
-       printf("You are %s", input);
+       printf("You are %s\n", input);
+
+       free(input);
    }
 }
+
