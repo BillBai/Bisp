82b872f2c82e574dc1357f4975c780817d48dec5
BillBai <BillBai42@gmail.com>
2014-10-18 15:47:28 +0800

    evaluation

--- main.c
+++ main.c
@@ -29,6 +29,35 @@ void add_history(char *input) {}
 
 #endif
 
+long eval_op(long x, char* op, long y)
+{
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    if (strcmp(op, "%") == 0) { return x % y; }
+    return 0;
+}
+
+long eval(mpc_ast_t* t)
+{
+
+    if (strstr(t->tag, "number")) { return atoi(t->contents); }
+
+    // The operator is always the third child
+    char* op = t->children[1]->contents;
+
+    // Get the first operand
+    long x = eval(t->children[2]);
+
+    // iterate the reamining operands
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+    return x;
+}
 
 int main(int argc, char** argv)
 {
@@ -57,18 +86,19 @@ int main(int argc, char** argv)
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Bisp, &r))
         {
-           mpc_ast_print(r.output);
+           long result = eval(r.output);
+           printf("%li\n", result);
            mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
         }
 
-
-
         free(input);
     }
 
     mpc_cleanup(4, Number, Operator, Expr, Bisp);
+
+    return 0;
 }
 
